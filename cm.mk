437626318975ec3b76858b371717d41cfb64f119
Nico Kaiser <kaiser.nico@gmail.com>
2012-11-07 14:16:04 +0100

    saga: update build fingerprint and manufaturer
    Change-Id: I7dfb058cd52585c5eaa0054c655fc9b112cd0c45

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_FINGERPRINT=htc_europe/htc_saga/saga:2.3.5/GRJ90/156318.4:user/release-keys PRIVATE_BUILD_DESC="2.10.401.4 CL156318 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_FINGERPRINT=htc_europe/htc_saga/saga:4.0.4/IMM76D/434411.2:user/release-keys PRIVATE_BUILD_DESC="14.01.401.2 CL434411 release-keys"
 
 PRODUCT_NAME := cm_saga
 PRODUCT_DEVICE := saga
